3560f33d23a730d682973b7972110d0b08c084cf
talhajilal <talhajilal@gmail.com>
2022-12-28 23:44:18 -0500

    New

--- hello.sh
+++ hello.sh
@@ -1,26 +1,20 @@
 #/bin/bash
 echo "Hello World"
 backup_dir=/opt/backup_dir
-
-main() {
-  echo "this shouldn't run"
-  echo "this shouldn't run either"
-}
-
 create_dir() {
     mkdir -p $backup_dir
     mkdir -p $backup_dir/sql_backup
 }
-copy_webfile() {
-    cp -R /moodledata/cache  $backup_dir/.
-    cp -R /moodledata/localcache $backup_dir/.
-    cp -R /moodledata/sessions $backup_dir/.
-    cp -R /moodledata/temp $backup_dir/.
-    cp -R /moodledata/trashdir $backup_dir/.
-}
+# copy_webfile() {
+#     cp -R /moodledata/cache  $backup_dir/.
+#     cp -R /moodledata/localcache $backup_dir/.
+#     cp -R /moodledata/sessions $backup_dir/.
+#     cp -R /moodledata/temp $backup_dir/.
+#     cp -R /moodledata/trashdir $backup_dir/.
+# }
 
 main() {
     create_dir
-    copy_webfile
+    # copy_webfile
 }
 main
